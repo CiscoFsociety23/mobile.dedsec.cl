63c24f93575de19aef0ebb04b01e1cb34da37432
 Marco Figueroa <marcofigueroa23@icloud.com>
2024-09-04 20:45:43 -0400

    feature: implementation widget and containers

--- app/src/main/java/com/dedsec/dedsec_mobile/MainActivity.java
+++ app/src/main/java/com/dedsec/dedsec_mobile/MainActivity.java
@@ -1,24 +1,40 @@
 package com.dedsec.dedsec_mobile;
 
 import android.os.Bundle;
+import android.widget.*;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CheckBox checkBox;
+    private RadioGroup radioGroup;
+    private RadioButton radioButton1, radioButton2;
+    private ProgressBar progressBar;
+    private RatingBar ratingBar;
+    private ImageView imageView;
+    private Spinner spinner;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Enlazar los elementos de la vista con las variables en Java
+        checkBox = findViewById(R.id.checkbox);
+        radioGroup = findViewById(R.id.radioGroup);
+        radioButton1 = findViewById(R.id.radioButton1);
+        radioButton2 = findViewById(R.id.radioButton2);
+        progressBar = findViewById(R.id.progressBar);
+        ratingBar = findViewById(R.id.ratingBar);
+        imageView = findViewById(R.id.imageView);
+        spinner = findViewById(R.id.spinner);
+        recyclerView = findViewById(R.id.recyclerView);
     }
 }
