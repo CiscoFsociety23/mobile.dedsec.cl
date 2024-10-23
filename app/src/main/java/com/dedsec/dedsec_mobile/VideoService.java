ce9a683f58904195e049a47c9a001e8c574b2d22
 Marco Figueroa <marcofigueroa23@icloud.com>
2024-10-23 20:03:51 -0300

    feature: adding media content

--- app/src/main/java/com/dedsec/dedsec_mobile/VideoService.java
+++ app/src/main/java/com/dedsec/dedsec_mobile/VideoService.java
@@ -1,24 +1,83 @@
 package com.dedsec.dedsec_mobile;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+//Importo librerias de video
+import android.net.Uri;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+//Importo librerias para Video View
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+//Importo liberias de Sonido
+import android.media.MediaPlayer;
+import android.view.View;
 
 public class VideoService extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_video_service);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        //Configuramos el video para la reproduccion local
+        VideoView videoView = findViewById(R.id.videoView);
+
+        //Construimos una URI del video
+        String videoPath = "android.resource://"+ getPackageName() + "/" + R.raw.trailer;
+
+        //Convierto la cadena en un URI
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+
+        //Agrego controles de reproducción del video
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+
+        //Vinculamos el controlador de video para mostrar en la app
+        mediaController.setAnchorView(videoView);
+        videoView.start();
+
+        //Configuracion del WebView para cargar video de Yotube
+        WebView webView = findViewById(R.id.webView);
+
+        //Obtengo la configuración del WebView
+        WebSettings webSettings = webView.getSettings();
+
+        webSettings.setJavaScriptEnabled(true);
+        //Agrego la URL del Video en formato embed
+        String videoUrl = "https://www.youtube.com/embed/QtZPjL_CbQI";
+
+        //Cargo el webView en la app
+        webView.setWebViewClient(new WebViewClient());
+
+        //Cargar la URL en el video del webView
+        webView.loadUrl(videoUrl);
+
+        //Configuración de reproducción del sonido MP3
+        findViewById(R.id.reproducir).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Creo un MediaPlayer para reproducir el sonido
+                MediaPlayer mediaPlayer = MediaPlayer.create(VideoService.this, R.raw.sonido);
+
+                //Inicio la reproducción
+                mediaPlayer.start();
+
+                //Listener para liberar recursos cuando sonido termine
+                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mediaPlayer) {
+                        mediaPlayer.release(); //Libero recursos cuando el sonido termine
+                    }
+                });
+            }
         });
+
     }
 }
